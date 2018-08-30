c07970826ecfb394fe8a503659e1beb50e818d6a
yanjinquan <yanjinquan@vipgangqin.com>
2018-08-30 15:50:04 +0800

    my first commit.

--- RebasePractice/RebasePractice/ViewController.swift
+++ RebasePractice/RebasePractice/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+/**
+     my first commit.
+     /**
+     
+        */
+ */
 
 }
 
