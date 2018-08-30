d5d01171b964625409f46ae5e5b8b9e2fd2a342a
yanjinquan <yanjinquan@vipgangqin.com>
2018-08-30 15:38:55 +0800

    change to Swift

---
+++ RebasePractice/RebasePractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RebasePractice
+//
+//  Created by yanjinquan on 2018/8/30.
+//  Copyright © 2018年 Yanjianquan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
